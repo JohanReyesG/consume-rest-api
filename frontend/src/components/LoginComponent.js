e9954a652f0582d32484acf4cdc8d1f0c4b7ebb0
JohanReeysG <reyesjohan665@gmail.com>
2024-05-16 17:56:56 -0500

    fix

--- frontend/src/components/LoginComponent.js
+++ frontend/src/components/LoginComponent.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 function LoginComponent({ onLogin }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-
+    const [error, setError] = useState(false);
     const handleLogin = async () => {
         try {
+            setError(false)
             const response = await axios.post('http://localhost:3001/login', { username, password });
             const accessToken = response.data.accessToken;
             localStorage.setItem('accessToken', accessToken);
@@ -13,13 +14,14 @@ function LoginComponent({ onLogin }) {
             onLogin();
 
         } catch (error) {
+            setError(true)
             console.error('Error de inicio de sesión:', error.response.data);
         }
     };
 
     return (
         <div className="login-container">
-
+            {error && <span className='error-msg'>Credenciales incorrectas</span>}
             <h1>Iniciar sesión</h1>
             <input
                 type="text"
